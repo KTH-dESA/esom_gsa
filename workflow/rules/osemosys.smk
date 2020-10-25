9dbf6044798761a86920ec4b9baaead0d7fe9afb
Will Usher <wusher@kth.se>
2020-10-25 22:40:13 +0100

    Add rules and script to create modelruns from sample file

--- workflow/rules/osemosys.smk
+++ workflow/rules/osemosys.smk
@@ -1,6 +1,31 @@
+wildcard_constraints:
+    modelrun="\d+"
+
+rule copy_datapackage:
+    message: "Copying datapackage for '{output.folder}'"
+    input:
+        "resources/gcc_india/datapackage.json"
+    output:
+        folder=directory("processed_data/gcc_india_{model_run, \d+}"),
+        dummy="processed_data/gcc_india_{model_run, \d+}/datapackage.json"
+    shell:
+        "otoole convert datapackage datapackage {input} {output.folder}"
+
+rule generate_datafile:
+    message: "Modifying data and generating datafile for '{output}'"
+    input:
+        datapackage="processed_data/gcc_india_{model_run, \d+}/datapackage.json"
+        sample="modelruns/{model_run, \d+}_sample.txt"
+    output:
+        "modelruns/gcc_india_{model_run}.txt"
+    shell:
+        "python workflow/scripts/create_modelrun.py {input.datapackage} {output} {input.sample}"
+
 rule generate_lp_file:
-    message: "Generating the LP file for '{wildcards.model_run}'"
-    input: data="resources/simplicity_{model_run}.txt", model=config['model_file']
+    message: "Generating the LP file for '{output}'"
+    input: 
+        data=expand("modelruns/gcc_india_{{model_run}}.txt"),
+        model=config['model_file']
     output:
         protected("results/{model_run}.lp.gz")
     log:
@@ -11,7 +36,7 @@ rule generate_lp_file:
         "glpsol -m {input.model} -d {input.data} --wlp {output} --check --log {log}"
 
 rule solve_lp:
-    message: "Solving the LP for '{wildcards.model_run}'"
+    message: "Solving the LP for '{output}'"
     input:
         "results/{model_run}.lp.gz"
     output:
@@ -24,12 +49,12 @@ rule solve_lp:
         "cbc {input} -dualpivot pesteep -psi 1.0 -pertv 52 -duals solve -solu {output} > {log}"
 
 rule process_solution:
-    message: "Processing CBC solution for '{wildcards.model_run}'"
+    message: "Processing CBC solution for '{output}'"
     input:
         solution="results/{model_run}.sol",
-        data="resources/simplicity_{model_run}.txt"
+        data="processed_data/gcc_india_{model_run}/datapackage.json"
     output: expand("results/{{model_run}}/{result}.csv", result=RESULTS.index)
     params:
         folder="results/{model_run}"
     shell:
-        "mkdir -p {params.folder} && otoole results cbc csv {input.solution} {params.folder} --input_datafile {input.data}"
+        "mkdir -p {params.folder} && otoole results cbc csv {input.solution} {params.folder} --input_datapackage {input.data}"
---
+++ workflow/scripts/create_modelrun.py
@@ -0,0 +1,100 @@
+"""Example data processing script
+"""
+import sys
+
+from typing import Dict, List, Union
+
+import csv
+import pandas as pd
+import numpy as np
+from otoole.read_strategies import ReadDatapackage
+from otoole.write_strategies import WriteDatafile
+
+
+def read_data(list_of_filepaths):
+    df = pd.read_csv()
+
+    return df
+
+def write_data(df, list_of_filepaths):
+    """Write out data to disk
+    """
+    pass
+
+def process_data(df: pd.DataFrame, index: List, value: float, 
+                 first_year: int, last_year: int) -> pd.DataFrame:
+    """Return the interpolated data between min and max years
+    """
+    df.index = df.index.sortlevel(level=0)[0]
+    df = df.loc[tuple(index + [first_year]):tuple(index + [last_year])]
+    df = df.reset_index().set_index('YEAR')
+    df.loc[last_year, 'VALUE'] = value
+    df.loc[first_year + 1:last_year - 1, 'VALUE'] = np.nan
+    print(df)
+    result = df.astype({'VALUE':'float'}).interpolate(method='values')
+    print(result)
+    return result.reset_index().set_index(['REGION', 'TECHNOLOGY', 'YEAR'])
+
+class TestInterpolate:
+
+    def test_interpolate_parameter(self):
+
+        index = "GLOBAL,GCPSOUT0N".split(",")
+        value = 6000.0
+        data = [
+            ['GLOBAL', 'GCPSOUT0N', 2015, 1000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2016, 1000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2017, 1000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2018, 1000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2019, 1000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2020, 1000.0],
+            ['GLOBAL', 'GCPXXXT0N', 2020, 1000.0],
+        ]
+
+        df = pd.DataFrame(data, columns=['REGION', 'TECHNOLOGY', 'YEAR', 'VALUE']
+                          ).set_index(['REGION', 'TECHNOLOGY', 'YEAR']).astype({'VALUE': float})
+        
+        actual = process_data(df, index, value, 2015, 2020)
+
+        data = [
+            ['GLOBAL', 'GCPSOUT0N', 2015, 1000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2016, 2000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2017, 3000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2018, 4000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2019, 5000.0],
+            ['GLOBAL', 'GCPSOUT0N', 2020, 6000.0],
+        ]
+
+        expected = pd.DataFrame(data, columns=['REGION', 'TECHNOLOGY', 'YEAR', 'VALUE']
+                          ).set_index(['REGION', 'TECHNOLOGY', 'YEAR'])
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+
+def main(input_filepath, output_filepath, parameters: List[Dict[str, Union[str, int, float]]]):
+
+    reader = ReadDatapackage()
+    writer = WriteDatafile()
+
+    with open(parameters, 'r') as csv_file:
+        parameters = list(csv.DictReader(csv_file))
+    print(parameters)
+
+    model_params, default_values = reader.read(input_filepath)
+
+    first_year = model_params['YEAR'].min().values[0]
+    end_year = model_params['YEAR'].max().values[0]
+
+    for parameter in parameters:
+        name = parameter['name']
+        index = parameter['indexes'].split(",")
+        value = parameter['value']
+        df = model_params[name]
+        new_values = process_data(df, index, value, first_year, end_year)
+        model_params[name].update(new_values)
+
+    writer.write(model_params, output_filepath, default_values)
+
+if __name__ == "__main__":
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
