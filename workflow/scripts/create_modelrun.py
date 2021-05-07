70b22985968aecb30d557a66ea69c7703067a412
Will Usher <wusher@kth.se>
2021-05-07 21:00:50 +0200

    Added test for error with non-year params

--- workflow/scripts/create_modelrun.py
+++ workflow/scripts/create_modelrun.py
@@ -36,17 +36,16 @@ from logging import getLogger
 
 logger = getLogger(__name__)
 
-def process_data(df: pd.DataFrame, index: List,
-                 start_year_value: float,end_year_value: float,
-                 first_year: int, last_year: int
+def process_data(
+                 start_year_value: float,
+                 end_year_value: float,
+                 first_year: int,
+                 last_year: int
                  ) -> pd.DataFrame:
     """Interpolate data between min and max years
 
     Arguments
     ---------
-    df: pd.DataFrame
-    index: List[str]
-        List of index names e.g. ``['GLOBAL', 'GCPSOUT0N']``
     start_year_value: float
         Value of the parameter in the start year
     end_year_value: float
@@ -62,45 +61,29 @@ def process_data(df: pd.DataFrame, index: List,
                        np.array([int(first_year), int(last_year)]),
                        np.array([float(start_year_value), float(end_year_value)])).T
 
-    df.loc[tuple(index + [first_year]):tuple(index + [last_year])] = values
-    return df.loc[tuple(index + [first_year]):tuple(index + [last_year])]
+    # df.loc[tuple(index + [first_year]):tuple(index + [last_year])] = values
+    return values
 
 
 class TestInterpolate:
 
     def test_interpolate_parameter(self):
 
-        index = "GLOBAL,GCPSOUT0N".split(",")
         start_year_value = 1000.0
         end_year_value = 6000.0
-        data = [
-            ['GLOBAL', 'GCPSOUT0N', 2015, 1000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2016, 1000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2017, 1000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2018, 1000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2019, 1000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2020, 1000.0],
-            ['GLOBAL', 'GCPXXXT0N', 2020, 1000.0],
-        ]
 
-        df = pd.DataFrame(data, columns=['REGION', 'TECHNOLOGY', 'YEAR', 'VALUE']
-                          ).set_index(['REGION', 'TECHNOLOGY', 'YEAR']).astype({'VALUE': float})
+        actual = process_data(start_year_value, end_year_value, 2015, 2020)
 
-        actual = process_data(df, index, start_year_value, end_year_value, 2015, 2020)
+        expected = np.array([[
+            1000.0,
+            2000.0,
+            3000.0,
+            4000.0,
+            5000.0,
+            6000.0,
+        ]]).T
 
-        data = [
-            ['GLOBAL', 'GCPSOUT0N', 2015, 1000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2016, 2000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2017, 3000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2018, 4000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2019, 5000.0],
-            ['GLOBAL', 'GCPSOUT0N', 2020, 6000.0],
-        ]
-
-        expected = pd.DataFrame(data, columns=['REGION', 'TECHNOLOGY', 'YEAR', 'VALUE']
-                          ).set_index(['REGION', 'TECHNOLOGY', 'YEAR'])
-
-        pd.testing.assert_frame_equal(actual, expected)
+        np.testing.assert_equal(actual, expected)
 
 
 def get_types_from_tuple(index: list, param: str, config: Dict) -> Tuple:
@@ -135,29 +118,26 @@ def modify_parameters(
         df = model_params[name]
         untyped_index = parameter['indexes'].split(",")
         index = get_types_from_tuple(untyped_index, name, config)
-        start_year_value = parameter['value_base_year']
+        start_year_value = float(parameter['value_base_year'])
         end_year_value = parameter['value_end_year']
         action = parameter['action']
         inter_index = parameter['interpolation_index']
         if action == 'interpolate':
-            df2 = df.sort_index()
-            snippet = df2.xs(tuple(index), drop_level=False)
-            new_values = process_data(snippet, index, start_year_value, end_year_value, first_year, end_year)
+            new_values = process_data(start_year_value, end_year_value, first_year, end_year)
         elif action == 'fixed':
             if inter_index == 'None':
                 # Create new object and update inplace
-                data = [index + [start_year_value]]
+                data = [start_year_value]
             elif inter_index == 'YEAR':
                 # Create new object and update inplace
-                data = [index + [x] + [start_year_value] for x in range(first_year, end_year + 1, 1)]
-            columns = config[name]['indices']
-            new_values = pd.DataFrame(data, columns=columns + ['VALUE']).astype(config[name]['index_dtypes']).set_index(columns)
-        if all(new_values.index.isin(model_params[name].index)):
+                data = np.full((end_year + 1 - first_year, 1), start_year_value)
+            new_values = data
+        try:
             logger.info("Updating values for {} in {}".format(index, name))
-            model_params[name].update(new_values)
-        else:
-            logger.info("Appending values for {} in {}".format(index, name))
-            model_params[name] = model_params[name].append(new_values)
+            model_params[name].loc[tuple(index + [first_year]):tuple(index + [end_year])] = new_values
+        except pd.core.indexing.IndexingError as ex:
+            print(name)
+            raise pd.core.indexing.IndexingError(str(ex))
 
     return model_params
 
@@ -185,6 +165,7 @@ class TestModifyParameters:
             ["GLOBAL","AEIELEI0H",1,2016,100.0],
             ["GLOBAL","AEIELEI0H",1,2017,100.0],
             ["GLOBAL","AEIELEI0H",1,2018,100.0],
+            ["GLOBAL","AEIELEXXX",1,2015,100.0],
         ]
 
         model_params = {
@@ -208,8 +189,40 @@ class TestModifyParameters:
             ["GLOBAL","AEIELEI0H",1,2016,1.0],
             ["GLOBAL","AEIELEI0H",1,2017,1.0],
             ["GLOBAL","AEIELEI0H",1,2018,1.0],
+            ["GLOBAL","AEIELEXXX",1,2015,100.0],
         ], columns=var_cost_cols).astype(
                 {'REGION': 'str','TECHNOLOGY': 'str','MODE_OF_OPERATION': 'int','YEAR': 'int','VALUE': 'float'}
+            ).set_index(var_cost_index)
+        pd.testing.assert_frame_equal(actual[name], expected)
+
+    def test_fixed_none(self):
+        name = 'DiscountRate'
+        var_cost_cols = ['REGION','VALUE']
+        var_cost_index = ['REGION']
+        index_dtypes = {'REGION': 'str','VALUE': 'float'}
+        data = [
+            ["GLOBAL", 0.05],
+        ]
+        model_params = {
+            'DiscountRate': pd.DataFrame(data=data, columns=var_cost_cols).astype(
+                index_dtypes
+            ).set_index(var_cost_index),
+            'YEAR': pd.DataFrame(data=[2015, 2016, 2017, 2018], columns=['VALUE'])}
+        parameters = [{'name': 'DiscountRate',
+                       'indexes': "GLOBAL",
+                       'value_base_year':  0.1,
+                       'value_end_year': 0.1,
+                       'dist': 'unif',
+                       'interpolation_index': 'None',
+                       'action': 'fixed'}]
+
+        config = {'DiscountRate': {'indices': var_cost_index,
+                                   'index_dtypes': index_dtypes}}
+        actual = modify_parameters(model_params, parameters, config)
+        expected = pd.DataFrame(data=[
+            ["GLOBAL", 0.05],
+        ], columns=var_cost_cols).astype(
+                {'REGION': 'str', 'VALUE': 'float'}
             ).set_index(var_cost_index)
         pd.testing.assert_frame_equal(actual[name], expected)
 
