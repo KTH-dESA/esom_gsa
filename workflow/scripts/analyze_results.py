c8a388a00c64f08726c58b626aeb991ee1d16eab
trevorb1 <trevor_barnes@sfu.ca>
2022-09-09 11:51:48 +0200

    fixed plotting

--- workflow/scripts/analyze_results.py
+++ workflow/scripts/analyze_results.py
@@ -32,6 +32,8 @@ The ``inputs.txt`` should be the output from SALib.sample.morris.sample
 The ``model/results.csv`` must have an 'OBJECTIVE' column holding results
 
 """
+
+from math import ceil
 from SALib.analyze import morris as analyze_morris
 from SALib.plotting import morris as plot_morris
 import os
@@ -42,6 +44,7 @@ from typing import List
 import sys
 import utils
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
 from logging import getLogger
 
@@ -60,10 +63,20 @@ def main(parameters: dict, X: np.array, Y: np.array, save_file: str):
     fig = plt.figure(figsize=(16, 8), constrained_layout=True)
     subfigs = fig.subfigures(1, 2)
 
+    # chnage histogram labels to legend for clarity
+    problem_hist = problem.copy()
+    problem_hist['names'] = [f'X{x}' for x, _ in enumerate(problem_hist['names'])]
+    legend_labels = [f"{problem_hist['names'][num]} = {problem['names'][num]}" for num, _ in enumerate(problem['names'])]
+    legend_handles = [mlines.Line2D([],[], color='w', marker='.', linewidth=0, markersize=0, label=label) for label in legend_labels]
+
+    # plot histogram 
     axs_left = subfigs[0].subplots(1)
-    plot_morris.sample_histograms(subfigs[0], X, problem)
+    plot_morris.sample_histograms(subfigs[0], X, problem_hist)
     subfigs[0].patch.set_visible(False)
     axs_left.axis('off')
+    ncols = ceil(len(legend_labels)/2)
+    subfigs[0].legend(handles=legend_handles, ncol=ncols, markerscale=0, frameon=False, framealpha=1)
+    subfigs[0].suptitle(' ', fontsize=(ncols*20))
 
     axs_right = subfigs[1].subplots(2)
     plot_morris.horizontal_bar_plot(axs_right[0], Si, unit="(\$)")
